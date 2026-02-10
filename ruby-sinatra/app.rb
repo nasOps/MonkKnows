f846c6299b0347129d32d5163d757d0bc05d9b45
Sofie Broman <sofierytter@hotmail.com>
2026-02-10 17:15:18 +0100

    Api- and HTML-route for search implemented

--- ruby-sinatra/app.rb
+++ ruby-sinatra/app.rb
@@ -6,14 +6,42 @@ require 'json'
 require_relative 'config/environment'
 require_relative 'models/page'
 
+
 class WhoknowsApp < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+  # set is Sinatra Configuration-method
+  set :public_folder, File.expand_path('../public', __FILE__)
+  set :views, File.expand_path('../views', __FILE__)
 
-  get "/" do
+  # Test - no DB needed
+  get "/hello" do
     "Sinatra says Hello World!"
   end
 
+  # === HTML routes - for user interface (HTML-responses) ===
+
+  get "/" do
+    erb :index # This will render the views/index.erb file as the homepage
+  end
+
+  get '/search' do
+    @query = params[:q] # Gets query parameter from URL, e.g. /search?q=example
+    @language = params[:language] || 'en' # Default to English if no language is specified
+
+    if @query # Variable used in HTML-template
+      @results = Page.where(language: @language)
+                     .where("content LIKE ?", "%#{@query}%")
+    else
+      @results = []
+    end
+
+    erb :'search/results'
+  end
+
+  # === API routes (JSON responses) ===
+
+  # API endpoint for searching pages
   get '/api/search' do
     content_type :json
 
