96cbe88efd1cbd1d7fe6ee7b54424861c34b1f58
Sofie <sobr0002@stud.ek.dk>
2026-02-21 19:33:07 +0100

    Added linting to our CI-pipeline

--- ruby-sinatra/models/user.rb
+++ ruby-sinatra/models/user.rb
@@ -3,15 +3,14 @@ require 'digest'
 # User-model - mapper til 'users'-tabellen via ActiveRecord.
 # ActiveRecord finder automatisk tabellen ud fra klassenavnet (User => users).
 class User < ActiveRecord::Base
-
   # Validations - svarer til if/elsif-tjek i Flask's /api/register route
-  validates :username, presence: { message: "You have to enter a username" },
-                       uniqueness: { message: "The username is already taken" }
+  validates :username, presence: { message: 'You have to enter a username' },
+                       uniqueness: { message: 'The username is already taken' }
 
-  validates :email, presence: { message: "You have to enter a valid email address" },
-                    format: { with: /@/, message: "You have to enter a valid email address" }
+  validates :email, presence: { message: 'You have to enter a valid email address' },
+                    format: { with: /@/, message: 'You have to enter a valid email address' }
 
-  validates :password, presence: { message: "You have to enter a password" }
+  validates :password, presence: { message: 'You have to enter a password' }
 
   # MD5-hashing - samme som Flask's hashlib.md5()
   # Klassemetode (self.) - kaldes med User.hash_password("test")
@@ -21,6 +20,6 @@ class User < ActiveRecord::Base
 
   # Instansmetode - kaldes paa et user-objekt: user.verify_password("test")
   def verify_password(plain_password)
-    self.password == User.hash_password(plain_password)
+    password == User.hash_password(plain_password)
   end
 end
--- ruby-sinatra/spec/app_spec.rb
+++ ruby-sinatra/spec/app_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../app'
 require 'rack/test'
 
-RSpec.describe "Whoknows App" do
+RSpec.describe 'Whoknows App' do
   include Rack::Test::Methods # This module provides methods like `get`, `post`, etc. to simulate HTTP requests in tests.
 
   def app
     WhoknowsApp # Tells RSpec which app to test
   end
 
-  describe "GET /hello" do
-    it "returns 200 OK" do
-      get "/hello"
+  describe 'GET /hello' do
+    it 'returns 200 OK' do
+      get '/hello'
       expect(last_response.status).to eq(200)
     end
   end
