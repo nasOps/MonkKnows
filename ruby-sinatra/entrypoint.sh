e8628fed83d1748aa08dbf31ea46b203b9a6af32
Sofie <sobr0002@stud.ek.dk>
2026-04-24 12:17:29 +0200

    Corrected entrypoint.sh to migrate sqlite3 DB instead of creating one

--- ruby-sinatra/entrypoint.sh
+++ ruby-sinatra/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "Setting up logging database..."
-bundle exec ruby db/create_logging_db.rb
+echo "Running migrations..."
+bundle exec rake db:migrate
+
 exec bundle exec rackup config.ru -p 4567 -o 0.0.0.0
