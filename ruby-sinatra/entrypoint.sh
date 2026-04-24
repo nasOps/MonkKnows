442ae6de7d1285be42f7571f56d373fe886e5867
Sofie <sobr0002@stud.ek.dk>
2026-04-24 13:28:00 +0200

    Added skipping migration if already done in entrypoint.sh

--- ruby-sinatra/entrypoint.sh
+++ ruby-sinatra/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 echo "Running migrations..."
 bundle exec rake db:migrate
 
+echo "Running data migration (safe)..."
+bundle exec rake data:migrate_logs || true
+
 exec bundle exec rackup config.ru -p 4567 -o 0.0.0.0
