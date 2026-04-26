e90909649e916e05f104e8e3c60b980514722894
Sofie <sobr0002@stud.ek.dk>
2026-04-26 14:02:54 +0200

    Changed login register to expect FORM and not data

--- ruby-sinatra/db/schema.rb
+++ ruby-sinatra/db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_24_095720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "pages", primary_key: "title", id: :text, force: :cascade do |t|
-    t.text "url", null: false
-    t.text "language", default: "en", null: false
-    t.datetime "last_updated", precision: nil
-    t.text "content", null: false
-  end
-
   create_table "search_logs", force: :cascade do |t|
     t.string "query"
     t.string "path"
@@ -31,14 +24,5 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_24_095720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_search_logs_on_created_at"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.text "username", null: false
-    t.text "email", null: false
-    t.text "password"
-    t.text "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
