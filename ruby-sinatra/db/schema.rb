f32813517072db2f10441588b32d0004e7587cf9
Sofie <sobr0002@stud.ek.dk>
2026-04-26 15:05:54 +0200

    Downgraded ruby versions

--- ruby-sinatra/db/schema.rb
+++ ruby-sinatra/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_24_095720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
